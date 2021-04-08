5cc04929dee551c73a99a9dd02855c994a30853d
GitIsMyLife <351501@naver.com>
2021-04-08 18:17:36 +0900

    add pcm const

--- trans-session.js
+++ trans-session.js
@@ -1,3 +1,10 @@
+/* protocol control messages */
+const PCM_SET_CHUNK_SIZE = 1;
+const PCM_ABORT_MESSAGE = 2;
+const PCM_ACKNOWLEDGEMENT = 3;
+const PCM_WINDOW_ACKNOWLEDGEMENT = 5;
+const PCM_SET_PEER_BANDWIDTH = 6;
+
 class TRANS_SESSION {
 
 }
