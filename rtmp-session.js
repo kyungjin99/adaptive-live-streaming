ff0f1cf4149a2880e7a5f80c71b74196eb17ed0d
kyungjin99 <kyungjin.kim.cs@gmail.com>
2021-04-12 02:16:07 +0900

    Implement chunk parsing, add message handler

--- rtmp-session.js
+++ rtmp-session.js
@@ -1,3 +1,10 @@
+/* chunk parsing state */
+const PARSE_INIT = 0;
+const PARSE_BHEADER = 1;
+const PARSE_MHEADER = 2;
+const PARSE_EXTENDED_TIMESTAMP = 3;
+const PARSE_PAYLOAD = 4;
+
 /* chunk types */
 const CHUNK_TYPE_0 = 0; // timestamp: 3B, message length: 3B, message type: 1B, message stream id: 4B
 const CHUNK_TYPE_1 = 1; // timestamp: 3B, message length: 3B, message type: 1B
@@ -10,6 +17,20 @@ const PCM_ABORT_MESSAGE = 2;
 const PCM_ACKNOWLEDGEMENT = 3;
 const PCM_WINDOW_ACKNOWLEDGEMENT = 5;
 const PCM_SET_PEER_BANDWIDTH = 6;
+
+/* user control message */
+const USER_CONTROL_MESSAGE = 4;
+
+/* rtmp command messages */
+const COMMAND_MESSAGE_AMF0 = 20;
+const COMMAND_MESSAGE_AMF3 = 17;
+const DATA_MESSAGE_AMF0 = 18;
+const DATA_MESSAGE_AMF3 = 15;
+const SHARED_OBJECT_MESSAGE_AMF0 = 19;
+const SHARED_OBJECT_MESSAGE_AMF3 = 16;
+const AUDIO_MESSAGE = 8;
+const VIDEO_MESSAGE = 9;
+const AGGREGATE_MESSAGE = 22;
 
 const packet = {
   create: (fmt = 0, csid = 0) => {
@@ -34,7 +55,280 @@ const packet = {
 class RTMP_SESSION {
   constructor() {
     this.chunkSize = 128; // max bytes of data in a chunk (default 128)
+
+    // for parsing chunk
+    this.parsingState = 0;
+    this.bytesParsed = 0;
+    this.parsedChunkBuf = Buffer.alloc(18); // stores parsed chunk. assign a header size of 18 (MAX)
+    this.bheaderSize = 0; // chunk basic header size (in bytes)
+    this.mheaderSize = 0; // chunk message header size (in bytes)
+    this.useExtendedTimestamp = 0;
+    this.parsedPacket = packet.create(); // this will multiplex a message
   }
+
+  readChunks(data, readBytes, length) {
+    let dataOffset = 0; // current offset of a chunk data received
+
+    while (dataOffset < length) { // until finishing reading chunk
+      switch (this.parsingState) {
+        case PARSE_INIT: { // to parse a chunk basic header, you need to know how big it is
+          this.parsedChunkBuf[0] = data[readBytes + dataOffset]; // read 1 byte from data and write to buf
+          this.bytesParsed += 1;
+          const bheaderType = this.parsedChunkBuf[0] & 0X3F; // the bottom 6 bits of the first byte
+          switch (bheaderType) {
+            case 0: // chunk basic header 2
+              this.bheaderSize = 2;
+              break;
+            case 1: // chunk basic header 3
+              this.bheaderSize = 3;
+              break;
+            default: // chunk basic header 1
+              this.bheaderSize = 1;
+              break;
+          }
+          this.parsingState = PARSE_BHEADER; // move on to the next step
+          break;
+        }
+        case PARSE_BHEADER: { // parse chunk basic header
+          while (this.bytesParsed < this.bheaderSize && dataOffset < length) { // reading bheader
+            this.parsedChunkBuf[this.bytesParsed] = data[readBytes + dataOffset];
+            this.bytesParsed += 1;
+            dataOffset += 1;
+          }
+          if (this.bytesParsed >= this.bheaderSize) { // finished reading bheader: start parsing it
+            this.parseChunkBasicHeader(); // extract fmt and csid
+            this.parsingState = PARSE_MHEADER; // move on to the next step
+          }
+          break;
+        }
+        case PARSE_MHEADER: { // parse chunk message header
+          const { fmt } = this.parsedPacket.header.basicHeader; // chunk type
+          let endpoint = this.bheaderSize;
+          switch (fmt) {
+            case 0:
+              endpoint = 11;
+              break;
+            case 1:
+              endpoint = 7;
+              break;
+            case 2:
+              endpoint = 3;
+              break;
+            case 3:
+              endpoint = 0;
+              break;
+            default: break;
+          }
+          while (this.bytesParsed < endpoint && dataOffset < length) {
+            this.parsedChunkBuf[this.bytesParsed] = data[readBytes + dataOffset]; // reading mheader
+            this.bytesParsed += 1;
+            dataOffset += 1;
+          }
+          if (this.bytesParsed >= endpoint) {
+            this.parseChunkMessageHeader(); // extract timestamp, mlen, mtid, msid
+            this.parsingState = PARSE_EXTENDED_TIMESTAMP; // move on to the next step
+          }
+          break;
+        }
+        case PARSE_EXTENDED_TIMESTAMP: { // parse extended timestamp
+          let endpoint = this.bheaderSize + this.mheaderSize;
+          // check if uses extended timestamp
+          if (this.parsedPacket.header.chunkMessageHeader.timestamp === 0xFFFFFF) { // read from chunk
+            this.useExtendedTimestamp = 4;
+            endpoint += 4;
+            while (this.bytesParsed < endpoint && dataOffset < length) {
+              this.parsedChunkBuf[this.bytesParsed] = data[readBytes + dataOffset]; // reading extended timestamp
+              this.bytesParsed += 1;
+              dataOffset += 1;
+            }
+            if (this.bytesParsed >= endpoint) {
+              this.parsedPacket.header.chunkMessageHeader.timestamp = this.parsedChunkBuf.readUInt32BE(endpoint - 4);
+            }
+          }
+          this.parsingState = PARSE_PAYLOAD; // move on to the next step
+          break;
+        }
+        case PARSE_PAYLOAD: { // parse payload
+          const size = length - dataOffset;
+          data.copy(this.parsedPacket.payload, this.bytesParsed, dataOffset, dataOffset + size);
+          this.bytesParsed += size;
+          dataOffset += size;
+
+          const totalPayloadSize = this.bytesParsed - (this.bheaderSize + this.mheaderSize + this.useExtendedTimestamp);
+          if (totalPayloadSize >= this.parsedPacket.header.chunkMessageHeader.plen) {
+            this.parsingState = PARSE_INIT; // finished reading a chunk. restart the parsing cycle
+            dataOffset = 0;
+            // clear parsedPacket
+            this.bytesParsed = 0;
+            this.parsedPacket = packet.create();
+            this.callHandler();
+          }
+          break;
+        }
+        default: break;
+      }
+    }
+  }
+
+  parseChunkBasicHeader() { // fmt, csid
+    const { bheaderSize } = this;
+    const fmt = this.parsedChunkBuf[0] >> 6;
+    this.parsedPacket.header.basicHeader.fmt = fmt; // parse fmt
+
+    let csid = 0;
+    switch (bheaderSize) { // parse csid
+      case 1: { // chunk basic header 1
+        csid = this.parsedChunkBuf[0] & 0X3F;
+        break;
+      }
+      case 2: { // chunk basic header 2
+        csid = this.parsedChunkBuf.readUInt8(1) + 64;
+        break;
+      }
+      case 3: { // chunk basic header 3
+        csid = this.parsedChunkBuf.readUInt16BE(1) + 64;
+        break;
+      }
+      default: break;
+    }
+    this.parsedPacket.header.basicHeader.csid = csid;
+  }
+
+  parseChunkMessageHeader() {
+    const { fmt } = this.parsedPacket.header.basicHeader; // chunk type
+    let offset = this.bheaderSize;
+
+    // read timestamp (delta) field except for type3 chunks
+    if (fmt < CHUNK_TYPE_3) {
+      const timestamp = this.parsedChunkBuf.readUIntBE(offset, 3);
+      if (timestamp !== 0XFFFFFF) {
+        this.parsedPacket.header.chunkMessageHeader.timestamp = timestamp;
+      }
+      else { // uses extended timestamp
+        this.parsedPacket.header.chunkMessageHeader.timestamp = 0XFFFFFF;
+      }
+      offset += 3;
+      this.mheaderSize += 3;
+    }
+
+    // read message length and message stream id field for type 0, type 1 chunks
+    if (fmt < CHUNK_TYPE_2) {
+      this.parsedPacket.header.chunkMessageHeader.timestamp = this.parsedChunkBuf.readUIntBE(offset, 3);
+      offset += 3;
+      this.parsedPacket.header.chunkMessageHeader.mtid = this.parsedChunkBuf.readUInt8(offset);
+      offset += 1;
+      this.mheaderSize += 4;
+    }
+
+    // read message stream id for type 0 chunk
+    if (fmt === CHUNK_TYPE_0) {
+      this.parsedPacket.header.chunkMessageHeader.msid = this.parsedChunkBuf.readUInt32LE(offset);
+      offset += 4;
+      this.mheaderSize += 4;
+    }
+  }
+
+  // PROTOCOL CONTROL MESSAGES
+  callHandler() {
+    const { mtid } = this.parsedPacket.header.chunkMessageHeader.mtid;
+    const { payload } = this.parsedPacket;
+    switch (mtid) {
+      // protocol control message
+      case PCM_SET_CHUNK_SIZE:
+      case PCM_ABORT_MESSAGE:
+      case PCM_ACKNOWLEDGEMENT:
+      case PCM_WINDOW_ACKNOWLEDGEMENT:
+      case PCM_SET_PEER_BANDWIDTH:
+        this.callPCMHandler(mtid, payload);
+        break;
+      // user control message
+      case USER_CONTROL_MESSAGE:
+        this.callUCMHandler(mtid, payload);
+        break;
+      // rtmp command messages
+      case COMMAND_MESSAGE_AMF0:
+      case COMMAND_MESSAGE_AMF3:
+      case DATA_MESSAGE_AMF0:
+      case DATA_MESSAGE_AMF3:
+      case SHARED_OBJECT_MESSAGE_AMF0:
+      case SHARED_OBJECT_MESSAGE_AMF3:
+      case AUDIO_MESSAGE:
+      case VIDEO_MESSAGE:
+      case AGGREGATE_MESSAGE:
+        this.callRCMHandler(mtid, payload);
+        break;
+      default: break;
+    }
+    return null;
+  }
+
+  callPCMHandler(mtid, payload) {
+    switch (mtid) {
+      case PCM_SET_CHUNK_SIZE: {
+        this.setChunkSize(payload);
+        break;
+      }
+      case PCM_ABORT_MESSAGE: {
+        this.abortMessage(payload);
+        break;
+      }
+      case PCM_ACKNOWLEDGEMENT: {
+        this.sendACK(payload);
+        break;
+      }
+      case PCM_WINDOW_ACKNOWLEDGEMENT: {
+        this.setWindowACKSize(payload);
+        break;
+      }
+      case PCM_SET_PEER_BANDWIDTH: {
+        // extract windowSize(4B) and limitType(1B) from payload
+        const windowSize = this.parsedPacket.payload.readUInt32BE(0, 4);
+        const limitType = this.parsedPacket.payload.readUInt8(4);
+        this.setPeerBandwidth(windowSize, limitType);
+        break;
+      }
+      default:
+        break;
+    }
+    return null;
+  }
+
+  callUCMHandler(mtid, payload) {
+    return null;
+  }
+
+  callRCMHandler(mtid, payload) {
+    return null;
+  }
+
+  setChunkSize(size) {
+    this.chunkSize = size;
+    // write to socket
+  }
+
+  abortMessage(csid) {
+    return csid; // test
+    // write to socket
+  }
+
+  sendACK(sequenceNumber) {
+    return sequenceNumber; // test
+    // write to socket
+  }
+
+  setWindowACKSize(windowSize) {
+    return windowSize; // test
+    // write to socket
+  }
+
+  setPeerBandwidth(windowSize, limitType) {
+    return { windowSize, limitType }; // test
+    // write to socket
+  }
+
+  // USER CONTROL MESSAGES
+
+  // RTMP COMMAND MESSAGES
 
   createChunkBasicHeader(bheader) {
     const { fmt, csid } = bheader;
@@ -91,7 +385,7 @@ class RTMP_SESSION {
       }
     }
 
-    // add message length and message stream id field for type 0, type 1 chunks
+    // add message length and message type id field for type 0, type 1 chunks
     if (ctype < CHUNK_TYPE_2) {
       buf.writeUIntBE(mlen, 3, 3); // message length
       buf.writeUInt8(mtid, 6); // message type id
